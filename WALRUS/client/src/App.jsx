8e6aa8f799abc88520c1c87d1d2704b12ad38fa7
WyattS09 <wyatt.schraeder@gmail.com>
2026-01-21 13:36:13 -0600

    fixing home

--- WALRUS/client/src/App.jsx
+++ WALRUS/client/src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
+import Home from './views/Home'
 import Host from './views/Host'
 import Player from './views/Player'
 import Display from './views/Display'
-
+import Questions from './views/Questions'
 
 export default function App() {
-const role = new URLSearchParams(window.location.search).get('role')
-if (role === 'host') return <Host />
-if (role === 'display') return <Display />
-return <Player />
+  const roleParam = new URLSearchParams(window.location.search).get('role')
+
+  // Projector / whiteboard view
+  if (roleParam === 'display') return <Display />
+
+  const [view, setView] = useState('home')
+
+  if (view === 'host') return <Host />
+  if (view === 'player') return <Player />
+  if (view === 'questions') return <Questions />
+
+  return <Home setView={setView} />
 }
